37d93a3143f353215dd553a88c7032ec2ba6f5c7
ORKAN_BARAN_CEYLAN <orkanceylan19@icloud.com>
2024-05-16 11:07:17 +0200

    dingsbums

---
+++ src/labTasks/abstractFurniture.java
@@ -0,0 +1,14 @@
+package labTasks;
+public abstract class abstractFurniture implements IFurniture {
+    protected int x;
+    protected int y;
+    public void setX(int x) {this.x = x;}
+    @Override
+    public int getX() {return this.x;}
+    @Override
+    public void setY(int y) {this.y = y;}
+    @Override
+    public int getY() {return this.y;}
+
+
+}
